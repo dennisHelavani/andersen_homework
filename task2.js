0fb08331f6444d6cc9349d7499451cb5bc3fb749
DennisHelavani <dengelovani@gmail.com>
2022-06-14 10:54:28 +0300

    first homework is commited

---
+++ task2.js
@@ -0,0 +1,23 @@
+alert('Задание №2 Поиск суммы и частного');
+let work = true;
+while (work === true) {
+  let num1 = prompt('Введите ваше первое число', 'Ваше значение');
+  if (isNaN(num1) || num1 === '') {
+    console.log('Некорректный ввод!');
+    work = false;
+    break;
+  }
+  let num2 = prompt('Введите ваше второе число', 'Ваше значение');
+  if (isNaN(num2) || num2 === '') {
+    console.log('Некорректный ввод!');
+    work = false;
+    break;
+  } else {
+    console.log(
+      `Ответ: ${parseInt(num1) + parseInt(num2)}, ${(
+        parseInt(num1) / parseInt(num2)
+      ).toFixed(1)}`
+    );
+    work = false;
+  }
+}
