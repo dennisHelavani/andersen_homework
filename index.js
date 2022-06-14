00dc2270c1d41a7536e860103f14f555c0ec024c
DennisHelavani <dengelovani@gmail.com>
2022-06-14 10:15:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+alert('Задание №1');
+let num1 = prompt('Введите ваше первое число', 'Ваше значение');
+let num2 = prompt('Введите ваше второе число', 'Ваше значение');
+
+if (isNaN(num1) || isNaN(num2) || num1 === '' || num2 === '') {
+  console.log('Некорректный ввод!');
+} else {
+  console.log(parseInt(num1).toString(num2));
+}
